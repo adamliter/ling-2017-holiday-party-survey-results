a5f2bd7255873982f6df39a13161642c6731ee97
Adam Liter <io@adamliter.org>
2017-11-19 22:27:00 -0500

    Add table for alternative date suggestions

--- 2017LingHolidayPartySurveyResults/app.R
+++ 2017LingHolidayPartySurveyResults/app.R
@@ -11,7 +11,10 @@ ui <- fluidPage(
        br(),
        DT::dataTableOutput("oneOnlyTable"),
        br(),
-       DT::dataTableOutput("considerationsTable")
+       DT::dataTableOutput("alternativesTable"),
+       br(),
+       DT::dataTableOutput("considerationsTable"),
+       br()
        )
 )
 
@@ -23,7 +26,17 @@ server <- function(input, output) {
         mutate(`Thursday, December 7` = ifelse(grepl("December 7", data[[2]]), 1, 0),
                `Friday, December 8` = ifelse(grepl("December 8", data[[2]]), 1, 0),
                `Saturday, December 9` = ifelse(grepl("December 9", data[[2]]), 1, 0),
-               `Monday, December 11` = ifelse(grepl("December 11", data[[2]]), 1, 0))
+               `Monday, December 11` = ifelse(grepl("December 11", data[[2]]), 1, 0)) %>%
+        mutate(other = str_replace_all(data[[2]],
+                                       "(Thursday, December 7 \\(non-optimal because of modality and acquisition meetings Friday morning\\)|Friday, December 8 \\(non-optimal because there is a colloquium, but the speaker could come\\?!\\?\\)|Saturday, December 9|Monday, December 11 \\(last day of classes, but Sergio and Aaron \\(and possibly others\\?\\) will already be gone\\))",
+                                       "")) %>%
+        mutate(other = str_replace_all(other,
+                                       "(, )*",
+                                       "")) %>%
+        mutate(other = ifelse(other == "", NA, other))
+
+    other_dates <- data.frame(alternatives = data[["other"]][!is.na(data[["other"]])],
+                              stringsAsFactors = FALSE)
 
     comments <- data.frame(considerations = data[[3]][!is.na(data[[3]])],
                            stringsAsFactors = FALSE)
@@ -64,6 +77,13 @@ server <- function(input, output) {
                                               extensions = c("ColReorder", "Responsive"),
                                               options = list(dom = "t"),
                                               caption = "Number of people who would only attend on the indicated day")
+
+   output$alternativesTable <- DT::renderDataTable({other_dates},
+                                                   server = FALSE,
+                                                   rownames = FALSE,
+                                                   extensions = c("Responsive"),
+                                                   options = list(dom = "t"),
+                                                   caption = "Alternative suggestions for dates")
 
    output$considerationsTable <- DT::renderDataTable({comments},
                                                      server = FALSE,
